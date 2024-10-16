6539e69c86924c63deebc22b52978ad2d546e189
davidchang613 <davidchang613@gmail.com>
2024-10-16 00:05:02 -0400

    Create shared.groovy

---
+++ jenkins/lib/shared.groovy
@@ -0,0 +1,20 @@
+def readVariablesFromFile(Map input) {
+
+  def varMap = [:]
+
+  varMap = readVariablesFromFile('', varMap)
+  return varMap
+}
+
+def readVariablesFromFile(String fileName, Map varMap) {
+  try {
+    def file = readFile fileName
+    for (line in file.readLines()) {
+      item = line.split('=')
+      varMap[item[0]] = item[1]
+    }
+  } catch(Exception e) {
+    print e
+  }
+  return varMap
+}
