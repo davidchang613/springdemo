6781d5b16b7e47fbcf6691862a7a0ad82792f3fc
davidchang613 <davidchang613@gmail.com>
2024-10-17 00:16:41 -0400

    try same path

---
+++ shared.groovy
@@ -0,0 +1,25 @@
+def readVariablesFromFile(Map input) {
+
+  def varMap = [:]
+
+  varMap = readVariablesFromFile('', varMap)
+  return varMap
+}
+
+def readVariablesFromFile(String fileName, Map varMap) {
+  try {
+    def file = readFile fileName
+    for (line in file.readLines()) {
+      item = line.split('=')
+      varMap[item[0]] = item[1]
+    }
+  } catch(Exception e) {
+    print e
+  }
+  return varMap
+}
+
+def getSelf() {
+  echo 'shared.groovy'
+  return 'shared.groovy'
+}
